cfec4f486e8ef3a9271f63b909dcb16e2c925a02
paul <syrovatskyi93@gmail.com>
2020-12-24 19:34:28 +0200

    finish project v1

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Provider } from 'react-redux'
+import {createStore} from "redux";
+import {reducer} from "./store/reducers";
+import Todo from './Todo/index';
+
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <Todo />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
